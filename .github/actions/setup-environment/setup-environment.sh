7cb470008c5b4d4568d3b5ca82166a6532bd53b1
donarturo11 <arturwrona91@gmail.com>
2025-07-25 00:29:22 +0200

    Fixing errors #5

--- .github/actions/setup-environment/setup-environment.sh
+++ .github/actions/setup-environment/setup-environment.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CWD=$(cd $(dirname $0); pwd}
+CWD=$(cd $(dirname $0); pwd)
 [[ $RUNNER_OS == 'Linux' ]] && sudo apt-get update && sudo apt-get install ninja-build -y
 [[ $RUNNER_OS == 'Windows' ]] && echo "C:\\mingw64\\bin" >> $GITHUB_PATH
 if [[ $RUNNER_OS == 'Windows' ]]; then
