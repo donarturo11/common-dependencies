2a7ef0f31f62ec59b3292b8ad025247424b7dff5
donarturo11 <arturwrona91@gmail.com>
2025-07-25 08:23:42 +0200

    Fixing errors #15

--- .github/actions/setup-environment/setup-environment.sh
+++ .github/actions/setup-environment/setup-environment.sh
@@ -4,9 +4,8 @@ CWD=$(cd $(dirname $0); pwd)
 [[ $RUNNER_OS == 'Windows' ]] && echo "C:\\mingw64\\bin" >> $GITHUB_PATH
 if [[ $RUNNER_OS == 'Windows' ]]; then
   echo "C:\\mingw64\\bin" >> $GITHUB_PATH
-  RUNNER_WORKSPACE=$(cygpath -m $RUNNER_WORKSPACE) >> $GITHUB_ENV
 fi
-ARTIFACTS_DIR=$RUNNER_WORKSPACE/artifacts
+ARTIFACTS_DIR=$GITHUB_WORKSPACE/artifacts
 mkdir -vp $ARTIFACTS_DIR
 echo ARTIFACTS_DIR=$ARTIFACTS_DIR >> $GITHUB_ENV
 bash $CWD/setup-vcpkg.sh
