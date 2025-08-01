4d852009c834508b9e2a51ab320c953c8b238d7f
donarturo11 <arturwrona91@gmail.com>
2025-08-01 08:54:16 +0200

    normalize paths

--- vcpkg-repo/build-all.sh
+++ vcpkg-repo/build-all.sh
@@ -2,11 +2,12 @@
 
 CWD=$(cd $(dirname $0); pwd)
 ARTIFACTS_DIR=${ARTIFACTS_DIR:-$CWD/artifacts}
+ARTIFACTS_DIR=${ARTIFACTS_DIR//\\//}
 [ ! -e $ARTIFACTS_DIR ] && mkdir $ARTIFACTS_DIR
 find $CWD -iname 'vcpkg.json' | while read -r _manifest
 do
   _cwd=$(dirname $_manifest)
   cd $_cwd
   vcpkg install
-  tar -vczf $ARTIFACTS_DIR/$(basename $_cwd).tar.gz *
+  tar -vczf ${ARTIFACTS_DIR}/$(basename $_cwd).tar.gz *
 done
