af767f4d28dcbc95bddb409e0c27e06a973d2f4a
donarturo11 <arturwrona91@gmail.com>
2025-08-01 08:19:40 +0200

    Put dependency sets to several directories

---
+++ vcpkg-repo/build-all.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+CWD=$(cd $(dirname $0); pwd)
+ARTIFACTS_DIR=${ARTIFACTS_DIR:-$CWD/artifacts}
+[ ! -e $ARTIFACTS_DIR ] && mkdir $ARTIFACTS_DIR
+find $CWD -iname 'vcpkg.json' | while read -r _manifest
+do
+  _cwd=$(dirname $_manifest)
+  cd $_cwd
+  vcpkg install --dry-run
+  tar -vczf $ARTIFACTS_DIR/$(basename $_cwd).tar.gz *
+done
