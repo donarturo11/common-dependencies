bb294b2d3e8be087989cbbad35c74f9faeb3542b
donarturo11 <arturwrona91@gmail.com>
2025-08-01 09:49:38 +0200

    Add subgroups for Github Actions

--- vcpkg-repo/build-all.sh
+++ vcpkg-repo/build-all.sh
@@ -7,6 +7,8 @@ find $CWD -iname 'vcpkg.json' | while read -r _manifest
 do
   _cwd=$(dirname $_manifest)
   cd $_cwd
+  echo "::group::Building repo for $(basename $_cwd)"
   vcpkg install
   tar -vczf ${ARTIFACTS_DIR}/$(basename $_cwd).tar.gz *
+  echo "::endgroup::"
 done
