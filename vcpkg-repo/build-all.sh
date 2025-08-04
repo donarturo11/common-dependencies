22f956722f32d0e47d0ab204a88d92d190ad38d5
donarturo11 <arturwrona91@gmail.com>
2025-08-04 13:18:53 +0200

    Fix bugs 2

--- vcpkg-repo/build-all.sh
+++ vcpkg-repo/build-all.sh
@@ -3,11 +3,13 @@
 CWD=$(cd $(dirname $0); pwd)
 ARTIFACTS_DIR=${ARTIFACTS_DIR:-$CWD/artifacts}
 [ ! -e $ARTIFACTS_DIR ] && mkdir -p $ARTIFACTS_DIR
-if [ $RUNNER_OS == "Windows" ]; then
-  export VCPKG_DEFAULT_BINARY_CACHE=${LOCALAPPDATA//\\//}/vcpkg/archives
-else
-  export VCPKG_DEFAULT_BINARY_CACHE=$HOME/.cache/vcpkg/archives
-fi
+#if [ $RUNNER_OS == "Windows" ]; then
+#  export VCPKG_DEFAULT_BINARY_CACHE=${LOCALAPPDATA//\\//}/vcpkg/archives
+#else
+#  export VCPKG_DEFAULT_BINARY_CACHE=$HOME/.cache/vcpkg/archives
+#fi
+export VCPKG_DEFAULT_BINARY_CACHE=${ARTIFACTS_DIR}/vcpkg/archives
+[ ! -e $VCPKG_DEFAULT_BINARY_CACHE ] && mkdir -p $VCPKG_DEFAULT_BINARY_CACHE
 echo "::notice::VCPKG_DEFAULT_BINARY_CACHE: $VCPKG_DEFAULT_BINARY_CACHE"
 if [ $RUNNER_OS == "Windows" ]
 then
