94f006cb16ddb2c9e41a11a66129454ff967f2cc
donarturo11 <arturwrona91@gmail.com>
2025-08-04 13:31:34 +0200

    Run debug

--- vcpkg-repo/build-all.sh
+++ vcpkg-repo/build-all.sh
@@ -3,11 +3,6 @@
 CWD=$(cd $(dirname $0); pwd)
 ARTIFACTS_DIR=${ARTIFACTS_DIR:-$CWD/artifacts}
 [ ! -e $ARTIFACTS_DIR ] && mkdir -p $ARTIFACTS_DIR
-#if [ $RUNNER_OS == "Windows" ]; then
-#  export VCPKG_DEFAULT_BINARY_CACHE=${LOCALAPPDATA//\\//}/vcpkg/archives
-#else
-#  export VCPKG_DEFAULT_BINARY_CACHE=$HOME/.cache/vcpkg/archives
-#fi
 export VCPKG_DEFAULT_BINARY_CACHE=${ARTIFACTS_DIR}/vcpkg/archives
 [ ! -e $VCPKG_DEFAULT_BINARY_CACHE ] && mkdir -p $VCPKG_DEFAULT_BINARY_CACHE
 echo "::notice::VCPKG_DEFAULT_BINARY_CACHE: $VCPKG_DEFAULT_BINARY_CACHE"
@@ -37,4 +32,4 @@ do
   echo "Filename: $_arch" >> Packages
   echo >> Packages
 done
-cp -av $VCPKG_DEFAULT_BINARY_CACHE $ARTIFACTS_DIR
+find $ARTIFACTS_DIR
