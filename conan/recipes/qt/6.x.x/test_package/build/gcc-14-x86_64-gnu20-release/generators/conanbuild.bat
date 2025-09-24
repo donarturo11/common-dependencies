c7ef1c54c0d861d5909ab37691ac65960085db33
donarturo11 <arturwrona91@gmail.com>
2025-09-24 16:44:18 +0200

    Remove unnecessary files

--- conan/recipes/qt/6.x.x/test_package/build/gcc-14-x86_64-gnu20-release/generators/conanbuild.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "%~dp0/conanbuildenv-release-x86_64.bat"
-call "%~dp0/conanrunenv-release-x86_64.bat"
--- conan/recipes/qt/6.x.x/test_package/build/gcc-14-x86_64-gnu20-release/generators/deactivate_conanbuild.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "%~dp0\deactivate_conanrunenv-release-x86_64.bat"
-call "%~dp0\deactivate_conanbuildenv-release-x86_64.bat"
